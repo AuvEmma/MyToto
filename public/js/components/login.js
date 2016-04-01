93ada876c0d443cdbcc58fc8a5dbdcf992d4a4ec
Emma Hou <emma.houy@gmail.com>
2016-04-01 16:02:47 -0400

    modal dialog styling

--- public/js/components/login.js
+++ public/js/components/login.js
@@ -36,34 +36,36 @@ const Login = React.createClass({
           <div className="modal-content">
             <div className="modal-header">
               <button type="button" className="close" data-dismiss="modal">×</button>
-
               <h4><span className="glyphicon glyphicon-lock" /> Login</h4>
-              <div id="loginform" className="modal-body">
-                <form role="form" onSubmit={this.handleSubmit}>
-                  <div className="form-group">
-
-                  <label htmlFor="inputEmail" className="sr-only"><span className="glyphicon glyphicon-shopping-cart" />Email address</label>
-                  <input ref="email" type="email" id="inputEmail" className="form-control"  placeholder="Email address" autofocus />
-
-                  <label htmlFor="inputPassword" className="sr-only"><span className="glyphicon glyphicon-shopping-cart" />Password</label>
-                  <input ref="password" type="password" id="inputPassword" className="form-control" placeholder="Password" />
-
-                  <button type="submit" className="btn btn-block">Login
-                    <span className="glyphicon glyphicon-ok" />
-                  </button>
-                  </div>
-                </form>
-              <div className="modal-footer">
-                <button type="submit" className="btn btn-danger btn-default pull-left" data-dismiss="modal">
-                <span className="glyphicon glyphicon-remove" /> Cancel
-                </button>
-                <p>Need <a href="#">help?</a></p>
-              </div>
-              {this.state.error && (
-              <p>Password and email do not match</p>
-              )}
-              </div>
             </div>
+
+            <div className="modal-body" id="loginform">
+              <form role="form" onSubmit={this.handleSubmit}>
+
+              <div className="form-group">
+                <label htmlFor="inputEmail" className="sr-only"><span className="glyphicon glyphicon-shopping-cart" />Email address</label>
+                <input ref="email" type="email" id="inputEmail" className="form-control"  placeholder="Email address" autofocus />
+
+                <label htmlFor="inputPassword" className="sr-only"><span className="glyphicon glyphicon-shopping-cart" />Password</label>
+                <input ref="password" type="password" id="inputPassword" className="form-control" placeholder="Password" />
+
+                <button type="submit" className="btn btn-block">Login
+                  <span className="glyphicon glyphicon-ok" />
+                </button>
+              </div>
+
+              </form>
+            </div>
+
+            <div className="modal-footer">
+              <button type="submit" className="btn btn-danger btn-default pull-left" data-dismiss="modal">
+                <span className="glyphicon glyphicon-remove" /> Cancel
+              </button>
+              <p>Need <a href="#">help?</a></p>
+            </div>
+            {this.state.error && (
+            <p>Password and email do not match</p>
+            )}
           </div>
         </div>
       </div>
