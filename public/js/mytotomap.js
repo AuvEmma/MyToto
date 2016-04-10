b99aa261020c9ebc295fee59f3aae61ead60f578
Emma Hou <emma.houy@gmail.com>
2016-04-10 14:45:51 -0400

    able to show private toto on map with pink marker

--- public/js/mytotomap.js
+++ public/js/mytotomap.js
@@ -112,7 +112,6 @@ function initMap() {
     })
   }
   setPublicMarker(map)
-  setPrivateMarker(map)
   function setPublicMarker(resultsMap){
     var addresses = JSON.parse(localStorage.publictoto)
     addresses.forEach((el)=>{
@@ -145,33 +144,6 @@ function initMap() {
       })
       resultsMap.addListener('click', function(){
         infowindow.close()
-      })
-    })
-  }
-  function setPrivateMarker(resultsMap){
-    var addresses = JSON.parse(localStorage.myToto)
-    addresses.forEach((el)=>{
-      var infowindow = new google.maps.InfoWindow;
-      var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h3 id="firstHeading" class="firstHeading">'+el.name+'</h3>'+
-      '<div id="bodyContent">'+
-      '<ul class="list-group">' +
-      '<li class="list-group-item">Location: '+el.location+ '</li><br>'+
-      '<li class="list-group-item">Open Yearround: '+el.open_yearround+ '</li><br>'+
-      '<li class="list-group-item">Handicap Accessible: '+el.handicap_accessible+ '</li><br>'+
-      '<li class="list-group-item">Borough: '+el.borough+ '</li><br>'+
-      '<li class="list-group-item">Comment: '+el.comments+ '</li><br>'+
-      '</ul>'+
-      '</div>'+
-      '</div>';
-      var latlng = new google.maps.LatLng(el.latitude, el.longitude);
-      var pmarker = new google.maps.Marker({
-        map: resultsMap,
-        position: latlng,
-        animation: google.maps.Animation.DROP,
-        icon: '../img/pinkmarker.png'
       })
     })
   }
